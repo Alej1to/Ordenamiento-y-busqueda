5787bd1d6cd0e43ca504fa7183e0d206c22f6813
Alej1to <sanres-1977@hotmail.com>
2024-02-13 16:04:29 -0500

    Ahora si está completo, creo

---
+++ src/ordenamiento_busqueda/Busqueda.java
@@ -0,0 +1,21 @@
+package ordenamiento_busqueda;
+
+public class Busqueda {
+	public static Integer Binario(Integer[] data, Integer valor) {
+        int li = 0;
+        int ls = data.length;
+        while (li <= ls) {
+            int lm = (ls - li) / 2 + li;
+            if (data[lm] < valor) {
+                li = lm + 1;
+            } else if (data[lm] > valor) {
+                ls = lm - 1;
+            } else {
+                return lm;
+            }
+        }
+        return null;
+    }
+}
+
+
